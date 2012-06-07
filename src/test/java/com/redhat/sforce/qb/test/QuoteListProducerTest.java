3ba0648f3f7b04197ab16f53e96a4bb76c90249c
John Herson <jherson@ix.netcom.com>
2012-06-07 17:41:26 -0400

    adding test classes

---
+++ src/test/java/com/redhat/sforce/qb/test/QuoteListProducerTest.java
@@ -0,0 +1,15 @@
+package com.redhat.sforce.qb.test;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class QuoteListProducerTest {
+
+	
+	@Test
+	public void testRegister() throws Exception {
+		
+	}
+}
