80aac44a60ec4d31ce921c3569dfff7fc9160314
John Herson <jherson@ix.netcom.com>
2012-08-14 23:27:46 -0400

    fixes for the display of chatter feed items

--- src/main/scripts/jherson-ws_deploy.sh
+++ src/main/scripts/jherson-ws_deploy.sh
@@ -6,4 +6,4 @@ TARGET_DIR=$HOME/workspace/sfa-qb/target
 rm  $DEPLOYMENT_DIR/*
 cp $TARGET_DIR/quotebuilder.war $DEPLOYMENT_DIR
 touch $DEPLOYMENT_DIR/quotebuilder.war.dodeploy
-rsync -az $DEPLOYMENT_DIR/quotebuilder.war.dodeploy $DEPLOYMENT_DIR/quotebuilder.war jherson-ws.devlab.phx1.redhat.com:$JBOSS_HOME/standalone/deployments
+rsync -az $DEPLOYMENT_DIR/quotebuilder.war.dodeploy $DEPLOYMENT_DIR/quotebuilder.war jherson@jherson-ws.devlab.phx1.redhat.com:$JBOSS_HOME/standalone/deployments
