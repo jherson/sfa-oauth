ec1b37a4a7ba2eb99ee4b060a8120f9f0e4acf0c
John Herson <john.d.herson@gmail.com>
2013-12-18 13:07:30 -0500

    completed work on abstract class

--- src/main/java/com/nowellpoint/oauth/AuthenticationCallback.java
+++ src/main/java/com/nowellpoint/oauth/AuthenticationCallback.java
@@ -1,6 +1,21 @@
 package com.nowellpoint.oauth;
 
-public abstract interface AuthenticationCallback {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-	void handle(AuthenticationContext context);
+import com.nowellpoint.oauth.model.Identity;
+import com.nowellpoint.oauth.model.Token;
+
+public abstract class AuthenticationCallback {
+
+	public abstract void handle(AuthenticationContext context);
+	
+	public void init(HttpServletRequest request, HttpServletResponse response, Token token, Identity identity) {
+		AuthenticationContext context = new AuthenticationContext();
+		context.setRequest(request);
+		context.setResponse(response);
+		context.setIdentity(identity);
+		context.setToken(token);
+		handle(context);
+	}
 }
